2dd6be01dcc138cf1ce1d9a3be8f7eed751166d6
Brandon <bmushori@gmail.com>
2021-08-07 10:39:54 +0200

    Added Viewers and Videos

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import ImgSlider from "./ImgSlider"
+import Viewers from "./Viewers"
 
 
 const Home = (props) => {
     return (
         <Container>
             <ImgSlider />
+            <Viewers />
         </Container>
     ) 
 };
